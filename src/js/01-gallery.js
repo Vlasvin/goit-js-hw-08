cf545ef111d52e0aa14a3c108a47101ac079317d
Oleksii <103183090+Vlasvin@users.noreply.github.com>
2023-09-10 19:13:03 +0300

    task_01_complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
-console.log('HI');
+
+const imgContainer = document.querySelector('.gallery');
+const cardMarkup = createImgCard({ galleryItems });
+
+imgContainer.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createImgCard({ galleryItems }) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
