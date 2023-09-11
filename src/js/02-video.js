4f4250a8318e9b81016bff5f2d06efcc701d2acc
Oleksii <103183090+Vlasvin@users.noreply.github.com>
2023-09-11 19:47:09 +0300

    task_02_ completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const throttleTime = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+player.on('timeupdate', throttle(throttleTime, 1000));
+
+player
+  .setCurrentTime(savedTime)
+  .then(function (savedTime) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
